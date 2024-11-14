824656473f0c6dc84f86ddac695cfabcd8670adf
Shrutika Anil Sutrave <shrutikasutrave1693@gmail.com>
2024-11-14 17:40:28 -0500

    faulty calculator

---
+++ faulty_calculator.py
@@ -0,0 +1,23 @@
+num1= int(input('enter num1:'))
+num2= int(input('enter num1:'))
+op= input('enter operator:')
+
+if op == '+':
+    if (num1 ==56 and num2== 9) or (num1 ==9 and num2== 56):
+        print('77')
+    else:
+        print(num1+num2)
+if op == '*':
+    if (num1 ==45 and num2== 3) or (num1 ==3 and num2== 45):
+        print('555')
+    else:
+        print(num1*num2)
+if op == '/':
+    if (num1 ==56 and num2== 6) or (num1 ==6 and num2== 56):
+        print('4')
+    else:
+        print(num1/num2)
+
+
+
+
